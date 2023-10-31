7f97ebd981b96c032647f8b9287e918078a6b012
jalvsaker <ec2-user@ip-172-31-36-180.eu-west-1.compute.internal>
2023-10-31 17:55:45 +0000

    terraform and cloudwatch

--- infra/main.tf
+++ infra/main.tf
@@ -16,13 +16,38 @@ resource "aws_cloudwatch_dashboard" "main" {
             "account_count.value"
           ]
         ],
-        "period": 300,
+        "period": 5,
         "stat": "Maximum",
         "region": "eu-west-1",
         "title": "Total number of accounts"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.student_name}",
+            "bank_sum.value"
+          ]
+        ],
+        "period": 5,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Bank Sum"
       }
     }
   ]
 }
 DASHBOARD
+}
+
+module "alarm" {
+  source = "./alarm_module"
+  alarm_email = var.alarm_email
+  prefix = var.student_name
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,8 @@
 variable "student_name" {
   type = string
+  default = "jeal004"
+}
+
+variable "alarm_email" {
+  type = string
 }
